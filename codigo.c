dba0960c8301a3e1962e742a2d3a8b5248f40c4e
Jesusmaik1 <jesus.cova@uabc.edu.mx>
2026-04-14 10:29:13 -0700

    Update codigo.c

--- codigo.c
+++ codigo.c
@@ -3,6 +3,15 @@
 int main ()
 {
   int opc:
+  float n1,n2,res:
   
-  
+  Printf("Ingrese el primer numero ");
+  Scanf("%f",&n1);
+
+  printf("Ingrese el sengundo numero ");
+  scanf("%f",&n2);
+
+  res= n1+n2;
+
+  printf("resultado: %2.f
 }
